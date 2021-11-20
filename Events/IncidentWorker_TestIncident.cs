a58aba999f8214a6fb87fae2f2419ae02890a586
muturgan <caxap_8787@mail.ru>
2021-11-20 22:37:51 +0300

    a TestIncident worker was added

---
+++ Events/IncidentWorker_TestIncident.cs
@@ -0,0 +1,41 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Intrigue.Events
+{
+    public class IncidentWorker_TestIncident : IncidentWorker
+    {
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            Map map = (Map)parms.target;
+
+            if (map.mapPawns.ColonistsSpawnedCount < 2) {
+                Log.Message($"no no {map.mapPawns.ColonistsSpawnedCount}");
+                return false;
+            }
+            Log.Message($"yes yes {map.mapPawns.ColonistsSpawnedCount}");
+
+            return true;
+            // return base.CanFireNowSub(parms);
+        }
+
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            Map map = (Map)parms.target;
+
+            if (map.mapPawns.ColonistsSpawnedCount < 2)
+            {
+                Log.Message($"no no no {map.mapPawns.ColonistsSpawnedCount}");
+                return false;
+            }
+            Log.Message($"yes yes yes {map.mapPawns.ColonistsSpawnedCount}");
+
+            // return base.TryExecuteWorker(parms);
+            return true;
+        }
+    }
+}
