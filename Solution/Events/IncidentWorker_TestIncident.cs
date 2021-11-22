5495357c6cb499e74fa6214fc0968593c1c68b3b
muturgan <caxap_8787@mail.ru>
2021-11-23 01:16:28 +0300

    a project structure updated

--- Solution/Events/IncidentWorker_TestIncident.cs
+++ Solution/Events/IncidentWorker_TestIncident.cs
@@ -1,41 +1,107 @@
 ﻿using RimWorld;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Verse;
 
 namespace Intrigue.Events
 {
-    public class IncidentWorker_TestIncident : IncidentWorker
+    public class Item
     {
-        protected override bool CanFireNowSub(IncidentParms parms)
-        {
-            Map map = (Map)parms.target;
-
-            if (map.mapPawns.ColonistsSpawnedCount < 2)
-            {
-                Log.Message($"no {map.mapPawns.ColonistsSpawnedCount}");
-                return false;
-            }
-            Log.Message($"yes {map.mapPawns.ColonistsSpawnedCount}");
-
-            return true;
-            // return base.CanFireNowSub(parms);
+        public Item(string settlementGuid, string pawnGuid) {
+            this.settlementGuid = settlementGuid;
+            this.pawnGuid = pawnGuid;
         }
 
+        public readonly string settlementGuid;
+        public readonly string pawnGuid;
+    }
+
+    public interface IRoyaltyLeader {
+        int GetMaxPsylinkLevel();
+    }
+
+    public class IncidentWorker_TestIncident : IncidentWorker
+    {
         protected override bool TryExecuteWorker(IncidentParms parms)
         {
-            Map map = (Map)parms.target;
+            Log.Message("Start!");
 
-            if (map.mapPawns.ColonistsSpawnedCount < 2)
-            {
-                Log.Message($"no no {map.mapPawns.ColonistsSpawnedCount}");
-                return false;
-            }
-            Log.Message($"yes yes {map.mapPawns.ColonistsSpawnedCount}");
+            var settlements = Find.WorldObjects.Settlements;
+            Log.Message($"Settlement count is: {settlements.Count()}");
 
-            // return base.TryExecuteWorker(parms);
+            settlements.ForEach((s) => {
+                Log.Message($"Settlement guid: {s.GetUniqueLoadID()}");
+
+                var f = s.Faction;
+                Log.Message($"Settlement Faction name: {f.Name}");
+                Log.Message($"Settlement Faction def fixedName: {f.def.fixedName}");
+                Log.Message($"Settlement Faction def categoryTag: {f.def.categoryTag}");
+                Log.Message($"Settlement Faction guid: {f.GetUniqueLoadID()}");
+
+                var l = f.leader;
+                if (l != null) {
+                    Log.Message($"leader is here");
+                    Log.Message($"{l.Name}");
+
+                    if (ModLister.RoyaltyInstalled)
+                    {
+                        Log.Message($"MaxPsylinkLevel {l.GetMaxPsylinkLevel()}");
+                    }
+
+                    Log.Message($"TraderCaravanRole {l.GetTraderCaravanRole()}");
+                    Log.Message($"guid {l.GetUniqueLoadID()}");
+                }
+
+
+                var map = s.Map;
+                if (map != null) {
+                    Log.Message($"map is here");
+                    var isPlayerHome = map.IsPlayerHome;
+                    Log.Message($"isPlayerHome: {isPlayerHome}");
+
+                    var colonists = map.mapPawns.FreeColonists;
+                    Log.Message($"colonists count is: {colonists.Count()}");
+                    if (colonists.Count() > 0)
+                    {
+                        Log.Message($"Pawn guid: {colonists.First().GetUniqueLoadID()}");
+                        Log.Message($"TRY");
+                        var fc = colonists.First();
+
+                        Log.Message($"first colonist is here");
+                        Log.Message($"{fc.Name}");
+
+                        Log.Message($"RoyaltyInstalled {ModLister.RoyaltyInstalled}");
+                        if (ModLister.RoyaltyInstalled)
+                        {
+                            Log.Message($"MaxPsylinkLevel {fc.GetMaxPsylinkLevel()}");
+                        }
+
+                        Log.Message($"TraderCaravanRole {fc.GetTraderCaravanRole()}");
+                        Log.Message($"guid {fc.GetUniqueLoadID()}");
+
+                        var mySkill = fc.skills.skills[fc.skills.skills.Count() - 2];
+                        Log.Message($"skillLabel {mySkill.def.skillLabel}");
+                        Log.Message($"level {mySkill.Level}");
+                        Log.Message($"type def label {mySkill.def.label}");
+
+                        var lead = fc.Faction.TryGenerateNewLeader();
+                        Log.Message($"lead {lead}");
+                    }
+                }
+
+                Log.Message("=====================");
+                //if (!map.IsPlayerHome) {
+                /*
+                    var item = new Item(
+                        s.GetUniqueLoadID(),
+                        map.mapPawns.FreeColonists.First()?.GetUniqueLoadID()
+                    );
+                    items.Add(item);
+                */
+                //}
+            });
+
+            //Log.Message($"{items}");
+
             return true;
         }
     }
